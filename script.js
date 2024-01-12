776fc750ad9ac4ee24862a4aad8dc4ef2f4e6406
tudor <tudorvladneacsu@outlook.com>
2024-01-12 17:44:46 +0000

    added some changes to the array

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function bar() {
   console.log("something barry is going on");
 
-  const some_array = ["helloes", "goodbye", "meow"];
+  const some_array = ["helloes", "goodbye", "meow", "asd", "fgh"];
 
   // return bar instead of foo
   return "bar";
